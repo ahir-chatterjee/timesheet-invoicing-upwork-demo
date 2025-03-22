c5b59f499e8c108e3af6332b02f7f6f98716001f
Ahir Chatterjee <ahirchatterjee@gmail.com>
2025-03-22 17:23:01 -0500

    fixed amplify config

---
+++ src/routes/+layout.js
@@ -0,0 +1,6 @@
+// This enables SPA mode for the static adapter
+export const prerender = false; // Disable prerendering since we have dynamic routes
+export const ssr = false;
+
+// Enable trailingSlash for consistency
+export const trailingSlash = 'never'; 
